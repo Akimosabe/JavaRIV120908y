3feb33e1424713a681e1dadea83905aa7e8182ce
Akimosabe <he7seven@gmail.com>
2022-12-04 16:40:31 +0500

    task 1000

---
+++ src/timus/task_1000/Main.java
@@ -0,0 +1,18 @@
+package timus.task_1000;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println();
+        int a = in.nextInt();
+
+        System.out.println();
+        int b = in.nextInt();
+
+        System.out.println(a + b);
+
+    }
+}

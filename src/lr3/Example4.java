1adfb7451d999a5ceef633166535ecf0c3a228dd
Akimosabe <he7seven@gmail.com>
2022-12-11 18:57:59 +0500

    lr3 4ex

--- src/lr3/Example4.java
+++ src/lr3/Example4.java
@@ -1,4 +1,36 @@
 package lr3;
-
+import java.util.Scanner;
 public class Example4 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("Первое число: ");
+        int number1 = in.nextInt();
+        System.out.println("Второе число: ");
+        int number2 = in.nextInt();
+        int min, max;
+        if (number1 > number2)
+        {
+
+            max = number1;
+            min = number2;
+
+        }
+        else
+        {
+            max = number2;
+            min = number1;
+        }
+            //for
+            for (int i = min; i <= max; i++)
+            {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            //while
+            while (min <= max){
+                System.out.print(min + " ");
+                min++;
+            }
+            System.out.println();
+    }
 }

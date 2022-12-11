1716d6bb2d075324beec435a10a1d62c6394aa6b
Akimosabe <he7seven@gmail.com>
2022-12-11 17:22:48 +0500

    lr3 1, 6, 9

--- src/lr3/Example1.java
+++ src/lr3/Example1.java
@@ -11,29 +11,21 @@ public class Example1 {
         System.out.println("Enter a number from 1 to 7");
         int days = in.nextInt();
         switch (days) {
-            case 1:
-                System.out.println("Monday");
+            case 1: System.out.println("Monday");
                 break;
-            case 2:
-                System.out.println("Tuesday");
+            case 2: System.out.println("Tuesday");
                 break;
-            case 3:
-                System.out.println("Wednesday");
+            case 3: System.out.println("Wednesday");
                 break;
-            case 4:
-                System.out.println("Thursday");
+            case 4: System.out.println("Thursday");
                 break;
-            case 5:
-                System.out.println("Friday");
+            case 5: System.out.println("Friday");
                 break;
-            case 6:
-                System.out.println("Saturday");
+            case 6: System.out.println("Saturday");
                 break;
-            case 7:
-                System.out.println("Sunday");
+            case 7: System.out.println("Sunday");
                 break;
-            default:
-                System.out.println("Error");
+            default: System.out.println("Error");
 
         }
     }

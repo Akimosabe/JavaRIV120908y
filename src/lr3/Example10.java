b119f217c740d7a4e9fc4da915ad6c039b270aad
Akimosabe <he7seven@gmail.com>
2022-12-11 20:44:59 +0500

    lr3 8, 10 ex

--- src/lr3/Example10.java
+++ src/lr3/Example10.java
@@ -1,7 +1,24 @@
 package lr3;
-
+import java.util.*;
 public class Example10  {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        int numbers[] = new int[10];
+        int numbersrev[] = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            numbers[i] = random.nextInt(100);
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+        Arrays.sort(numbers);
+        for (int i = 0; i < 10; i++)
+        {
+            numbersrev[i]=numbers[9-i];
+            System.out.print(numbersrev[i] + " ");
+        }
 
     }
 }
--- src/lr3/Example8.java
+++ src/lr3/Example8.java
@@ -1,4 +1,30 @@
 package lr3;
 
 public class Example8 {
+    public static void main(String[] args) {
+        char letter = 'A';
+        char [] alphabet = new char[10];
+
+        int i = 0;
+        while(i<10)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    letter++;
+                    break;
+                case 'E':
+                    letter++;
+                    break;
+                case 'I':
+                    letter++;
+                    break;
+            }
+            alphabet[i]=letter;
+            letter++;
+            System.out.print(alphabet[i] + " ");
+            i++;
+        }
+
+    }
 }

ba6766059f61f98729fc7749c072bf330587a681
Akimosabe <he7seven@gmail.com>
2022-12-19 17:48:28 +0500

    lr4 3fix

--- src/lr4/Example3.java
+++ src/lr4/Example3.java
@@ -3,26 +3,19 @@ package lr4;
 public class Example3 {
     public static void main(String[] args) {
         int [] [] x = new int [6][10];
-        int a = 0;
-        int b;
 
-        for (int i=0; i <6; i++ )
-        {
-            for (int j = 0; j < 10; j ++)
-            {
+        for (int i=0; i <6; i++ ) {
+            for (int j = 0; j < 10; j ++) {
                 x[i][j] = 2;
             }
         }
-        for (int i =0; i < 6; i++)
-        {
-            b = i+1;
-            for (int j = 0; j < 10; j ++)
-            {
+        for (int i =0; i < 6; i++) {
+
+            for (int j = 0; j < 10; j ++) {
                 System.out.print(x[i][j]+" ");
-                a++;
             }
             System.out.println();
-            a = 0;
+
         }
     }
 }

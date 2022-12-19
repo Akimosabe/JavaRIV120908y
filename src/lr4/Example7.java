517fb3dffc7626f97cfad4afb3c6f67de074289f
Akimosabe <he7seven@gmail.com>
2022-12-19 20:44:12 +0500

    lr4 7

--- src/lr4/Example7.java
+++ src/lr4/Example7.java
@@ -1,4 +1,28 @@
 package lr4;
-
 public class Example7 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 0;
+        int[][] massive = new int[a][b];
+
+        for(int i = 0; i < a; i++) {
+            if(i % 2 == 0) {
+                for (int j = 0; j < b; j++, c++) {
+                    massive[i][j] = c;
+                }
+            }
+            else {
+                for (int j = b-1; j >= 0; j--, c++) {
+                    massive[i][j] = c;
+                }
+            }
+        }
+        for(int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++){
+                System.out.print(massive[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }

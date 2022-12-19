a58eed93ecf8bada7e20de2c6959835e2fb0d2eb
Akimosabe <he7seven@gmail.com>
2022-12-19 19:25:23 +0500

    lr4 6?

--- src/lr4/Example6.java
+++ src/lr4/Example6.java
@@ -1,4 +1,40 @@
 package lr4;
 
+import java.util.Random;
+
 public class Example6 {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 5;
+        int[][] massiveMassive = new int[a][b];
+        Random random = new Random();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                massiveMassive[i][j] = random.nextInt(10);
+                System.out.print(massiveMassive[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------");
+        int delA = random.nextInt(a - 1);
+        int delB = random.nextInt(b - 1);
+
+        int[][] massiveMassive2 = new int[a - 1][b - 1];
+        for (int i = 0, s = 0; i < a - 1; s++) {
+            if (s != delA) {
+                for (int j = 0, k = 0; j < b - 1; k++) {
+                    if (k != delB) {
+                        massiveMassive2[i][j] = massiveMassive[s][k];
+                        System.out.print(massiveMassive2[i][j] + " ");
+                        j++;
+                    }
+                }
+                i++;
+                System.out.println();
+            }
+        }
+    }
 }
+

ae30548afd8a01ce70737d3ac1080e87b0029970
Akimosabe <he7seven@gmail.com>
2022-12-19 18:22:57 +0500

    lr4 4fix

--- src/lr4/Example4.java
+++ src/lr4/Example4.java
@@ -1,4 +1,22 @@
 package lr4;
 
 public class Example4 {
+    public static void main(String[] args) {
+        int [][] triangle = new int [5][];
+        for (int i = 0; i < 5; i++ ){
+            int j = i+1;
+            triangle[i] = new int[j];
+        }
+        for (int i =0; i < 5; i++){
+            for (int j = 0; j <i+1; j++){
+                triangle[i][j] = 5;
+            }
+        }
+        for (int i =0; i < 5; i++){
+            for (int j = 0; j < i+1; j++){
+                System.out.print(triangle[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }

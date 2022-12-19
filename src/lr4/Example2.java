59c5c3640faef35fe22ce34cc730429d09a129d4
Akimosabe <he7seven@gmail.com>
2022-12-19 16:18:38 +0500

    lr4 1-3

--- src/lr4/Example2.java
+++ src/lr4/Example2.java
@@ -1,4 +1,19 @@
 package lr4;
 
 public class Example2 {
+    public static void main(String[] args) {
+        int figure = 15;
+        int i ;
+        int j ;
+        int z=0;
+        for (i = 1 ; i <= figure; i++){
+
+            for (j = 0; j <=z; j++){
+                System.out.print("+");
+            }
+            z = z + 1;
+            System.out.println();
+            //убираю вывод нумерации, т. к. фигура ломается из-за цифр после 9
+        }
+    }
 }
--- src/lr4/Example3.java
+++ src/lr4/Example3.java
@@ -1,4 +1,30 @@
 package lr4;
 
 public class Example3 {
+    public static void main(String[] args) {
+        int y = 10; // w = y  h = x
+        int x = 6;
+        int [] [] z = new int [x][y];
+        int a = 0;
+        int b;
+
+        for (int i=0; i <x; i++ )
+        {
+            for (int j = 0; j < y; j ++)
+            {
+                z[i][j] = 2;
+            }
+        }
+        for (int i =0; i < x; i++)
+        {
+            b = i+1;
+            for (int j = 0; j <y; j ++)
+            {
+                System.out.print(z[i][j]+" ");
+                a++;
+            }
+            System.out.println();
+            a = 0;
+        }
+    }
 }
